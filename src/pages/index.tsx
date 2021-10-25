75d8cab6d6d01d032f1edf1ad0a28f2fb4c53ab8
Callyel Alves <callyelalves@hotmail.com>
2021-10-25 12:45:38 -0300

    add components SignInButton and SubscribeButton

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,7 @@
 import Head from "next/head";
+import { SubscribeButton } from "../components/SubscribeButton";
+import styles from "./home.module.scss";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -6,7 +9,23 @@ export default function Home() {
       <Head>
         <title>Home | TecNews</title>
       </Head>
-      <h1>Hello world</h1>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Olá, Seja bem vindo!</span>
+          <h1>
+            Noticias sobre <span>Tecnologia</span>
+          </h1>
+          <p>
+            Tenha acesso a conteudos de tecnologias <br />
+            <span>por R$ 9,99/mês</span>
+          </p>
+
+          <SubscribeButton />
+        </section>
+
+        <Image src="/images/avatar.png" alt="Avatar" width={300} height={300} />
+      </main>
     </>
   );
 }
