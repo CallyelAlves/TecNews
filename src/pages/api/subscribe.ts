9a3c356bf19463fb47afd056e1883d6596059f82
Callyel Alves <callyelalves@hotmail.com>
2021-10-27 11:14:26 -0300

    add config stripe

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,18 +1,47 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { stripe } from "../../services/stripe";
+import { query as q } from "faunadb";
 import { getSession } from "next-auth/client";
+import { fauna } from "../../services/faunadb";
+
+interface User {
+  ref: {
+    id: string;
+  };
+  data: {
+    stripe_customer_id: string;
+  };
+}
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default async (req: NextApiRequest, response: NextApiResponse) => {
+export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "POST") {
     const session = await getSession({ req });
 
-    const stripeCustomer = await stripe.customers.create({
-      email: session.user.email,
-    });
+    const user = await fauna.query<User>(
+      q.Get(q.Match(q.Index("user_by_email"), q.Casefold(session.user.email)))
+    );
+
+    let customerId = user.data.stripe_customer_id;
+
+    if (!customerId) {
+      const stripeCustomer = await stripe.customers.create({
+        email: session.user.email,
+      });
+
+      await fauna.query(
+        q.Update(q.Ref(q.Collection("users"), user.ref.id), {
+          data: {
+            stripe_customer_id: stripeCustomer.id,
+          },
+        })
+      );
+
+      customerId = stripeCustomer.id;
+    }
 
     const stripeCheckoutSession = await stripe.checkout.sessions.create({
-      customer: stripeCustomer.id,
+      customer: customerId,
       payment_method_types: ["card"],
       billing_address_collection: "required",
       line_items: [
@@ -27,9 +56,9 @@ export default async (req: NextApiRequest, response: NextApiResponse) => {
       cancel_url: process.env.STRIPE_CANCEL_URL,
     });
 
-    return response.status(200).json({ sessionId: stripeCheckoutSession.id });
+    return res.status(200).json({ sessionId: stripeCheckoutSession.id });
   } else {
-    response.setHeader("Allow", "POST");
-    response.status(405).end("Method not allowed");
+    res.setHeader("Allow", "POST");
+    res.status(405).end("Method not allowed");
   }
 };
