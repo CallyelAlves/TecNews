7e37c987ce0e24a23d12c1c7cb132b2f4ae181af
Callyel Alves <callyelalves@hotmail.com>
2021-10-27 13:14:03 -0300

    edit color

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,7 +5,9 @@ export function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <h1>TecNews</h1>
+        <h1>
+          Tec <span>News</span>
+        </h1>
         <nav>
           <a href="#" className={styles.active}>
             Home
